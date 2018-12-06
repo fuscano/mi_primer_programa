d908521503e94b207c194a844f8585895fbb2975
fuscano <jonafuscano1@gmail.com>
2018-12-06 13:28:16 +0000

    Añadiendo el archivo ejemplo while

---
+++ example_while.py
@@ -0,0 +1,11 @@
+
+
+numero_inicial = 11
+while numero_inicial>0:
+    numero_inicial -= 1
+    print(numero_inicial)
+    if numero_inicial%2==0:
+        print('este numero es par')
+    else:
+        print('este numero es impar')
+print('ha terminado la cuenta')
